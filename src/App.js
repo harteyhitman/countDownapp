8f9a2c6a9655515a78a5c171da3ebcf9e8326fc7
harteyhitman <oyegoke.a18@gmail.com>
2023-03-18 13:05:05 +0100

    update

--- src/App.js
+++ src/App.js
@@ -42,25 +42,25 @@ const Timer = () => {
         <div className="box">
           <p id="day">{days < 10 ? "0" + days : days}</p>
         </div>
-        <span className="text">Days <b>Abisola</b></span>
+        <span className="text">Days <b>(Abisola)</b></span>
       </div>
       <div className="col-4">
         <div className="box">
           <p id="hour">{hours < 10 ? "0" + hours : hours}</p>
         </div>
-        <span className="text">Hours <b>Mistura</b></span>
+        <span className="text">Hours <b>(Mistura)</b></span>
       </div>
       <div className="col-4">
         <div className="box">
           <p id="minute">{minutes < 10 ? "0" + minutes : minutes}</p>
         </div>
-        <span className="text">Minutes <b>Kiki</b></span>
+        <span className="text">Minutes <b>(Kiki)</b></span>
       </div>
       <div className="col-4">
         <div className="box">
           <p id="second">{seconds < 10 ? "0" + seconds : seconds}</p>
         </div>
-        <span className="text">Seconds <b>Oyegoke's</b></span>
+        <span className="text">Seconds <b>(Oyegoke's)</b></span>
       </div> 
       <div className="bottom">
         <img src={images} alt="" />
